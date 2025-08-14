e1220f52c29c549f35b26347d23da21efdcdfc9c
guswo4324 <phj2394324@naver.com>
2025-08-14 23:57:44 +0900

    rjust, ljust, center

--- 반복문/star2_rjust_ljust_center.py
+++ 반복문/star2_rjust_ljust_center.py
@@ -15,11 +15,5 @@ for i in range(1, n+1):
 for i in range(1, n+1):
     print(('*' * i).ljust(n, '-'))
 
-"""
-import sys
-
-n = int(sys.stdin.readline())
-
 for i in range(1, n+1):
-    print(('*' * i).rjust(n))
-"""
+    print(('*' * i).center(n, '-'))
