7d8819b4e147d8dd128e016a9ec89398e8d58394
guswo4324 <phj2394324@naver.com>
2025-08-21 18:17:29 +0900

    input balls in basket

---
+++ array/inputball.py
@@ -0,0 +1,26 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+basket = [0] * (N + 1)
+
+for _ in range(M):
+    start, end, num = map(int, sys.stdin.readline().split())
+    for i in range(start, end + 1):
+        basket[i] = num
+
+print(*basket[1:])
+
+"""
+바구니 N개
+
+1번 부터 N번가지 번호 매겨져있는 바구니
+
+1번부터 N번까지 번호가 적혀있는 공 
+
+첫째줄 N 입력
+둘째 줄 부터 M개의 줄에 걸쳐 공을 넣는 방법
+
+예시 2 5 6 이면 
+2번 부터 5번 바구니에 6번 공을 넣음
+"""
