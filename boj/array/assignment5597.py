21a77e786f06a4b23230092f6e7b384d23d7acab
guswo4324 <phj2394324@naver.com>
2025-08-22 20:58:33 +0900

    [boj] if not in & in, append

---
+++ boj/array/assignment5597.py
@@ -0,0 +1,47 @@
+"""
+[입력]
+3
+1
+4
+5
+7
+9
+6
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+
+[출력]
+2
+8
+"""
+
+import sys
+
+stduentnum = []
+
+for i in range(1, 29):
+    num = int(sys.stdin.readline())
+    stduentnum.append(num)
+
+for i in range(1, 31):
+    if i not in stduentnum:
+        print(i)
