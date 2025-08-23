19c8bab0e1b91ac7aea9b6d4cae2e55b4605fb2d
guswo4324 <phj2394324@naver.com>
2025-08-23 12:27:22 +0900

    [boj] reversed, ::-1 flip array

---
+++ boj/array/filpthebasket_reversed10811.py
@@ -0,0 +1,44 @@
+"""
+[입력]
+5 4
+1 2
+3 4
+1 4
+2 2
+
+[출력]
+3 4 1 2 5
+
+"""
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+basket_num = [0] * (N + 1)
+
+for item in range(1, N + 1):
+    basket_num[item] = item
+
+for _ in range(M):
+    i, j = map(int, sys.stdin.readline().split())
+    
+    start = i
+    end = j
+
+    while start < end:
+        temp = basket_num[start]
+        basket_num[start] = basket_num[end]
+        basket_num[end] = temp
+        start += 1
+        end -= 1
+
+"""
+< reveresed 또는 [::-1] 사용 >
+for _ in range(M):
+    i, j = map(int, sys.stdin.readline().split())
+    basket_num[i:j+1] = reversed(basket_num[i:j+1])
+    basket_num[i:j+1] = basket_num[i:j+1][::-1]
+"""
+
+print(*basket_num[1:])
