ac6a79b8532f933567320d3999c5379d3fe5d205
guswo4324 <phj2394324@naver.com>
2025-08-25 21:05:54 +0900

    [boj] the loop of string

---
+++ boj/string/loopstring2675.py
@@ -0,0 +1,36 @@
+"""
+[입력]
+2
+3 ABC
+5 /HTP
+
+[출력]
+AAABBBCCC
+/////HHHHHTTTTTPPPPP
+
+"""
+
+import sys
+
+T = int(sys.stdin.readline())
+
+result = []
+
+for _ in range(T):
+    R, word = sys.stdin.readline().split()
+    R = int(R)
+
+    #for char in word:
+        #print(char * R, end="")
+    #print()
+
+    new_word =""
+    for char in word:
+        new_word += char * R
+    
+    result.append(new_word)
+
+print('\n'.join(result))
+
+                  
+
