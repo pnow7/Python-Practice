6ca50dbdac9d5421ec1f8e0494b5b5991b2058c5
현재 <phj2394324@naver.com>
2025-08-30 22:26:29 +0900

    Fix error code location

--- boj/adv_lev1/star2444.py
+++ boj/adv_lev1/star2444.py
@@ -19,8 +19,14 @@ import sys
 
 N = int(sys.stdin.readline())
 
+for i in range(1, N + 1):
+    print(' ' * (N - i) + '*' * (2 * i - 1))
+
+for i in range(N - 1, 0, -1):
+    print(' ' * (N - i) + '*' * (2 * i - 1))
+
 """
-- 오류
+- 오류(끝 쪽 공백은 입력하지 않나봄)
 
 for i in range(1, N + 1):
     print(('*' * (2 * i - 1)).center(2 * N - 1))
@@ -29,9 +35,3 @@ for i in range(N - 1, 0, -1):
     print(('*' * (2 * i - 1)).center(2 * N - 1))
 
 """
-
-for i in range(1, N + 1):
-    print(' ' * (N - i) + '*' * (2 * i - 1))
-
-for i in range(N - 1, 0, -1):
-    print(' ' * (N - i) + '*' * (2 * i - 1))
