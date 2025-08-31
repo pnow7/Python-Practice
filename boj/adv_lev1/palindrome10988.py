3dc3cd0e81fa98063994236e9bd3782d39ce1876
현재 <phj2394324@naver.com>
2025-08-31 18:02:19 +0900

    [boj] 펠린드롬

---
+++ boj/adv_lev1/palindrome10988.py
@@ -0,0 +1,37 @@
+"""
+[입력]
+level
+
+baekjoon
+
+[출력]
+1
+
+0
+
+"""
+
+import sys
+
+word = list(sys.stdin.readline().strip())
+
+if len(word) % 2 != 0:
+    word.remove(word[len(word) // 2])
+
+reversed_word = list(reversed(word))
+
+# print(word, reversed_word)
+
+if word == reversed_word:
+    print(1)
+else:
+    print(0)
+
+
+
+
+
+
+
+
+
