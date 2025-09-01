5faf8532a6d4dba955ce6dc57bb361a0902ed382
현재 <phj2394324@naver.com>
2025-09-01 23:41:42 +0900

    [st] change file location

--- boj/adv_lev1/croatian_alphabet2941.py
+++ boj/adv_lev1/croatian_alphabet2941.py
@@ -25,16 +25,36 @@ dz=ak
 
 import sys
 
-croatian_alph = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+word = sys.stdin.readline().strip()
+croatian_alph = ['dz=', 'lj', 'nj', 'c=', 'c-', 'd-', 's=', 'z=']
 
-word = list(sys.stdin.readline().split())
+for char in croatian_alph:
+    word = word.replace(char, 'a')
 
+print(len(word))
+
+
+"""
+import sys
+
+word = sys.stdin.readline().strip()
 count = 0
+i = 0
 
-for items in word:
-    if items in croatian_alph:
+while i < len(word):
+    if i + 2 < len(word) and word[i:i+3] == 'dz=':
+        # 'dz='인 경우 (세 글자)
         count += 1
-
-
+        i += 3
+    elif i + 1 < len(word) and word[i:i+2] in ['c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z=']:
+        # 두 글자 크로아티아 알파벳인 경우
+        count += 1
+        i += 2
+    else:
+        # 그 외 나머지 경우 (일반 알파벳)
+        count += 1
+        i += 1
 
 print(count)
+
+"""
