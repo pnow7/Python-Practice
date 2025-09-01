179552c34e6ba10bfa304a0f28c4452e7363e8e4
현재 <phj2394324@naver.com>
2025-09-01 10:31:30 +0900

    remove del

--- study/0828/list.py
+++ study/0828/list.py
@@ -9,15 +9,23 @@ print(c)
 
 # 0번재 리스트에 삽입
 c.insert(0, '0번째 삽입')
+c.insert(1, '1번째 삽입')
 print(c)
 
 # 맨끝에 리스트 추가
-c.extend(['맨끝', '이걸삽입', 1])
+c.extend(['맨끝', '삽입', 1])
 print(c)
 
 # 0번재 인덱스 삭제
 c.remove(c[0])
 print(c)
+
+# 1번째 인덱스 삭제
+# (remove로 0번째 인덱스 삭제돼서 앞으로 땡겨져옴)
+del c[0]
+del c[1]
+print(c)
+
 print('**' * 20)
 
 # append()
