44a8bed67d96cc2e4149a21188a9e56f86545723
현재 <phj2394324@naver.com>
2025-09-03 18:50:05 +0900

    [boj] group word checker

--- boj/adv_lev1/group_word1316.py
+++ boj/adv_lev1/group_word1316.py
@@ -27,36 +27,27 @@ N = int(sys.stdin.readline())
 count = 0
 
 for _ in range(N):
-    alpha_check = [False] * 26
+    alpha_check = [False] * 26                                      # 기록용
     word = sys.stdin.readline().strip()
     
-    # 단어가 비어있거나 한 글자일 경우 그룹 단어로 처리
-    if len(word) <= 1:
+    if len(word) <= 1:                                              # 단어가 비어있거나 한 글자일 경우 그룹 단어로 처리
         count += 1
         continue
     
     is_group_word = True
+    alpha_check[ord(word[0]) - ord('a')] = True                     # 첫 번째 문자 먼저 기록
     
-    # 첫 번째 문자는 먼저 기록
-    # ord() 함수는 문자를 아스키코드로 변환
-    alpha_check[ord(word[0]) - ord('a')] = True
     
-    # 두 번째 문자부터 끝까지 반복하면서 검사
-    for i in range(1, len(word)):
+    for i in range(1, len(word)):                                   # 두 번째 문자부터 끝까지 반복하면서 검사
         current_char = word[i]
         prev_char = word[i-1]
         
-        # 현재 문자와 이전 문자가 다를 때
-        if current_char != prev_char:
-
-            # 현재 문자가 이전에 나온 적이 있는지 확인
-            if alpha_check[ord(current_char) - ord('a')] == True:
+        if current_char != prev_char:                               # 현재 문자와 이전 문자가 다를 때
+            if alpha_check[ord(current_char) - ord('a')] == True:   # 현재 문자가 이전에 나온 적이 있는지 확인
                 is_group_word = False
-                break  # 그룹 단어가 아니므로 반복 중단
-            
-            else:
-                # 처음 나온 문자이므로 기록
-                alpha_check[ord(current_char) - ord('a')] = True
+                break                                               # 그룹 단어가 아니므로 반복 중단
+            else:                                        
+                alpha_check[ord(current_char) - ord('a')] = True    # 처음 나온 문자이므로 기록
                 
     if is_group_word:
         count += 1
