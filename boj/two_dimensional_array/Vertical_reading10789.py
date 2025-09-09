0c747aa57561358fc5c7492869451367dee112c8
현재 <phj2394324@naver.com>
2025-09-09 23:36:26 +0900

    [boj] 2dim matrix

---
+++ boj/two_dimensional_array/Vertical_reading10789.py
@@ -0,0 +1,25 @@
+"""
+[입력]
+ABCDE
+abcde
+01234
+FGHIJ
+fghij
+
+[출력]
+Aa0FfBb1GgCc2HhDd3IiEe4Jj
+"""
+
+import sys
+
+word = [ list(sys.stdin.readline().strip()) for _ in range(5) ]
+
+result_string = ""
+
+# 세로로 읽기 때문에 i, j 반대로
+for j in range(15):
+    for i in range(5):
+        if len(word[i]) > j:
+            result_string += word[i][j] 
+
+print(result_string)
