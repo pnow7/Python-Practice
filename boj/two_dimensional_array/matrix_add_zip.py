a93efb0e00208ef54281dad073c5101acc92e6ed
현재 <phj2394324@naver.com>
2025-09-10 17:16:36 +0900

    comprehension

--- boj/two_dimensional_array/matrix_add_zip.py
+++ boj/two_dimensional_array/matrix_add_zip.py
@@ -2,6 +2,7 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
+# [ ] 안에 list 라서 2차원 행렬
 arr1 = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 arr2 = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
